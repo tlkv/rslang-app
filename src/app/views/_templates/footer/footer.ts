451cc853ba28de7eee25cc6197c5e530c4f757f9
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2022-02-07 20:55:55 +0300

    feat add home page

--- src/app/views/_templates/footer/footer.ts
+++ src/app/views/_templates/footer/footer.ts
@@ -1,9 +1,20 @@
 import './footer.scss';
 import Component from '../component';
+import '../../../../assets/github.png';
 
 class Footer extends Component {
-  elemContent = `footer 
-  content`;
+  elemContent = `<footer>
+      <p class="year-of-creation">@2022</p>
+      <div class="authors-box">
+          <img src="./assets/github.png" alt="github" class="github-img">
+          <a href="https://github.com/kristykov" class="author">Kristina</a>
+          <a href="https://github.com/AnastasiyaPoleshuk" class="author">Anastasiya</a>
+          <a href="https://github.com/tlkv" class="author">Andrey</a>
+      </div>
+      <a href="https://rs.school/js/">
+        <img src="https://rs.school/images/rs_school_js.svg" alt="rs.school" class="rs-school">
+      </a>
+    </footer>`;
 
   elemWrapper = new Component('div', ['container']);
 
--- src/app/views/_templates/header/header.ts
+++ src/app/views/_templates/header/header.ts
@@ -1,15 +1,23 @@
 import './header.scss';
 import Component from '../component';
 import defaultRoutes from '../../../models/router/defaultRoutes';
+import '../../../../assets/icon.png';
 
 class Header extends Component {
-  elemContent = `<a href="#${defaultRoutes.frontpage.path}">${defaultRoutes.frontpage.name}</a>
-  <a href="#${defaultRoutes.authorization.path}">${defaultRoutes.authorization.name}</a>
-  <a href="#${defaultRoutes.textbook.path}">${defaultRoutes.textbook.name}</a>
-  <a href="#${defaultRoutes.gameSprint.path}">${defaultRoutes.gameSprint.name}</a>
-  <a href="#${defaultRoutes.gameAudio.path}">${defaultRoutes.gameAudio.name}</a>
-  <a href="#${defaultRoutes.statistics.path}">${defaultRoutes.statistics.name}</a>
-  <a href="#${defaultRoutes.testpage.path}">${defaultRoutes.testpage.name}</a>`;
+  elemContent = `<nav class="navigation"> 
+        <div class="title"> 
+          <img src="./assets/icon.png" alt="icon" class="icon" /> 
+          <h1 class="h1">RS-Lang</h1> 
+        </div>       
+        <a href="#${defaultRoutes.frontpage.path}" class="menu-item">Home</a>
+        <a href="#${defaultRoutes.textbook.path}" class="menu-item">${defaultRoutes.textbook.name}</a>
+        <a href="#${defaultRoutes.gameSprint.path}" class="menu-item">${defaultRoutes.gameSprint.name}</a>
+        <a href="#${defaultRoutes.gameAudio.path}" class="menu-item">${defaultRoutes.gameAudio.name}</a>
+        <a href="#${defaultRoutes.statistics.path}" class="menu-item">${defaultRoutes.statistics.name}</a>
+        <a href="#${defaultRoutes.testpage.path}" class="menu-item">${defaultRoutes.testpage.name}</a>
+        <a href="#${defaultRoutes.authorization.path}" class="log-in">log-in</a>
+        <a href="#${defaultRoutes.authorization.path}" class="sign-out">sign out</a>
+      </nav>`;
 
   elemWrapper = new Component('div', ['container']);
 
