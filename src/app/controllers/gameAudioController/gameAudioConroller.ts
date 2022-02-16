3553cdba5a38514c51fa45992de6bf13a527c5d6
Krystsina Kavalevich <krikoax@gmail.com>
2022-02-16 21:40:23 +0300

    feat: add shuffle method

--- src/app/controllers/gameAudioController/gameAudioConroller.ts
+++ src/app/controllers/gameAudioController/gameAudioConroller.ts
@@ -1,7 +1,11 @@
 import GameAudioView from '../../views/gameAudioView/gameAudioView';
 import { State, state } from '../../models/api/state/state';
 import getWordsTextbook from '../../models/api/api/getWordsTextbook';
-import { FRONT_BLOCK_CONTENT_START, FRONT_BLOCK_CONTENT_GAME, FRONT_BLOCK_CONTENT_MODAL } from '../../views/gameAudioView/const';
+import {
+  FRONT_BLOCK_CONTENT_START,
+  FRONT_BLOCK_CONTENT_GAME,
+  FRONT_BLOCK_CONTENT_MODAL,
+} from '../../views/gameAudioView/const';
 import IAudioWord from '../../models/api/interfaces/IAudioWord';
 
 class GameAudioController {
@@ -33,9 +37,13 @@ class GameAudioController {
     this.view.frontBlock.container.addEventListener('click', async (e) => {
       if (!this.isGameStarted) {
         const startBtn = (e.target as HTMLElement).closest('#start-audio-btn') as HTMLInputElement;
-        const restartBtn = (e.target as HTMLElement).closest('#restart-audio-btn') as HTMLInputElement;
+        const restartBtn = (e.target as HTMLElement).closest(
+          '#restart-audio-btn',
+        ) as HTMLInputElement;
         if (startBtn) {
-          const checkedInput = document.querySelector('input[name="audio-level"]:checked') as HTMLInputElement;
+          const checkedInput = document.querySelector(
+            'input[name="audio-level"]:checked',
+          ) as HTMLInputElement;
           if (!checkedInput.value) {
             alert('Please select a level');
             return;
@@ -49,7 +57,56 @@ class GameAudioController {
       }
     });
   }
-  // async getWords(level: number, pageStart: number) {}
+
+  static shuffle(array: string[]) {
+    const arr = array;
+    // Fisher-Yates shuffle algorithm
+    // start from last element and go backwards
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      // pick random item from current end i to remaining items towards the start
+      const j = Math.floor(Math.random() * (i + 1));
+      // and swap
+      const temp = arr[i];
+      arr[i] = array[j];
+      arr[j] = temp;
+    }
+    return arr;
+  }
+
+  async getWords(level: number, pageStart: number) {
+    const words1 = await getWordsTextbook(level, pageStart);
+    this.model.words = words1;
+    const arr = [];
+    let usedWords = [];
+    for (let i = 0; i < words1.length; i += 1) {
+      const word = words1[i];
+      usedWords.push(word.wordTranslate);
+      // get 4 other words
+      while (usedWords.length < 5) {
+        // get a random word
+        let randomWord = words1[Math.floor(Math.random() * words1.length)].wordTranslate;
+        while (usedWords.includes(randomWord)) {
+          // if used word already contains a random word, then find another random word
+          randomWord = words1[Math.floor(Math.random() * words1.length)].wordTranslate;
+        }
+        usedWords.push(randomWord);
+      }
+      // all 5 words found
+      // shuffle used words
+      const shuffledWords = GameAudioController.shuffle(usedWords);
+      const correctIndex = shuffledWords.indexOf(word.wordTranslate);
+      const wordObj = {
+        audio: word.audio,
+        options: shuffledWords,
+        matchIndex: correctIndex,
+        image: word.image,
+      };
+      arr.push(wordObj);
+      usedWords = [];
+    }
+    // select 10 random words from arr
+    return arr.sort(() => Math.random() - Math.random()).slice(0, 10);
+  }
 }
 
 export default GameAudioController;
