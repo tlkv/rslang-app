869cf22c02f6f16177bb1fbafcad83018a10d6d2
Krystsina Kavalevich <krikoax@gmail.com>
2022-02-16 16:09:48 +0300

    style: restyle footer

--- src/app/views/_templates/footer/footer.ts
+++ src/app/views/_templates/footer/footer.ts
@@ -2,17 +2,22 @@ import './footer.scss';
 import Component from '../component';
 
 class Footer extends Component {
-  elemContent = `<footer>
-      <p class="year-of-creation">&copy;2022</p>
+  elemContent = `
+  <img class="footer-wave-img" src="../../../../../assets/wave-footer.svg">
+  <footer>
+      <p class="footer-title">Connect With Us</p>
       <div class="authors-box">
+      <a href="https://rs.school/js/">
+      <img src="https://rs.school/images/rs_school_js.svg" alt="rs.school" class="rs-school">
+    </a>
           <img src="./assets/github.png" alt="github" class="github-img">
           <a href="https://github.com/kristykov" class="author">Kristina</a>
           <a href="https://github.com/AnastasiyaPoleshuk" class="author">Anastasiya</a>
           <a href="https://github.com/tlkv" class="author">Andrey</a>
+      
+\
       </div>
-      <a href="https://rs.school/js/">
-        <img src="https://rs.school/images/rs_school_js.svg" alt="rs.school" class="rs-school">
-      </a>
+      <p class="year-of-creation">&copy;2022 RS Lang Limited. Registered in Belarus under Company Registration 56789</p>
     </footer>`;
 
   elemWrapper = new Component('div', ['container']);
