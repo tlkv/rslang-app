fae45a31069d371ed255a7d8dc2c35a6854479df
A0009 <aaa144855@gmail.com>
2022-02-20 15:50:03 +0300

    feat: add statistics for learned word

--- src/app/views/testView/testView.ts
+++ src/app/views/testView/testView.ts
@@ -2,6 +2,7 @@ import './testView.scss';
 import Component from '../_templates/component';
 import Header from '../_templates/header/header';
 import Footer from '../_templates/footer/footer';
+import IStats from '../../models/api/interfaces/IStats';
 
 class TestView extends Component {
   /* frontBlockContent = `<button class="auth-state">
@@ -24,6 +25,21 @@ class TestView extends Component {
     this.footer = new Footer(this.container);
     this.frontBlock.container.append(this.frontBlockWrapper.container);
     this.frontBlockWrapper.container.innerHTML = this.frontBlockContent;
+  }
+
+  renderStats(stats: IStats, isAuth: boolean) {
+    const uniqueKeys = Array.from(new Set(stats.optional?.wordList?.stat?.map((i) => i.wDate)));
+    console.log(uniqueKeys);
+    const graphData = uniqueKeys.map((elem) => ({
+      dateGraph: elem,
+      wordsAmount: stats.optional?.wordList?.stat?.filter((i) => i.wDate === elem).length,
+    }));
+
+    console.log(graphData);
+
+    const elemContent = `${JSON.stringify(graphData)}
+    <button class="reset-stats-button">Reset Stats</button>`;
+    this.frontBlockWrapper.container.innerHTML = elemContent;
   }
 }
 
